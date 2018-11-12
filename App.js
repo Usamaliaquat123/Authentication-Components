d7d37434196cf5ca274932f78a5ced13f7aad37d
Usamaliaquat123 <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2018-11-13 01:28:39 +0500

    😡

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import { View,Text} from 'react-native';
-import Input from "./src/components/passwordInput";
+import Input from "./src/components/Input";
 
 
 export default class App extends Component {
@@ -16,8 +16,8 @@ export default class App extends Component {
     return (
       <View>  
         <Text>Reuseable Components</Text>
-          <Input placeholder="ajsdh" type="password"/>
-            
+          <Input placeholder="email" type="email"/>
+          <Input placeholder="password" type="password" />
       </View>
     );
   }
